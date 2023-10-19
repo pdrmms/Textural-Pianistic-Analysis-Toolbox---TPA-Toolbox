e92e5c6bf5982b585d3bbbc0c96f1a781ad279fe
pdrmms <144931330+pdrmms@users.noreply.github.com>
2023-10-19 17:38:04 -0300

    Add files via upload

---
+++ tcbuilderXY.m
@@ -0,0 +1,33 @@
+%converte as pmps de up-space para tc-space. cada linha é uma das
+clc
+
+pmpbuilder;
+tc={}; tccodes=[];tccodesmirros=[];
+for f = 1:size(rec2)
+tc{f}=PMPtc(rec2{f});
+[code codemirror] =tccoder2classededistr(tc{f})
+tempcode=string(code); tempcodemirror=string(codemirror);
+tccodes=[tccodes;tempcode]; tccodesmirros=[tccodesmirros;tempcodemirror];
+end
+tc=tc';
+resposta=unique(tccodes,'stable');
+respostamirror=unique(tccodesmirros,'stable');
+sidebyside = [resposta respostamirror]
+
+temp=[];
+for f = 1:size(sidebyside,1)
+    rvez=sidebyside(f,2);
+    ind=find(rvez==sidebyside(:,1));
+    temp=[temp;ind];
+end
+%agora temos uma coluna com os códigos, e ao lado outra com os espelhos.
+% rastreamos onde os espelhos ocorrem na coluna normal.
+pos=1:size(temp,1);
+duo=[temp pos']%esse par é fundamental. se o elemento da esquerda for maior que o da direita, ele pode ser anulado. senão, deve ser mantido.
+
+alvo=find(duo(:,1)<duo(:,2))%aqui estamos pegando o alvo a ser preservado (esquerda é menor ou igual a direita, logo não pode ser excluído)
+
+temp(alvo)=[]%esvazia os índices a serem preservados
+resposta=resposta(temp) %filtra
+%tccodes=tccodes';
+%tccodes=string(tccodes)
